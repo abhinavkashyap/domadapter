1650ce095e134ee33a1e370837582da52c26ec26
Abhinav Ramesh Kashyap <abhinavkashyap92@gmail.com>
2021-08-19 17:30:16 +0800

    Build: Add pytorch-lightning data module for GLUE datasets
    - DataModule for GLUE datasets in `glue_dm.py` for loading of data
    - Tests for preparing, setting up data and  dataloaders for GLUE
      datasets. Tests are written in `ward`
    - .envrc manages project wide exports. Separate .envrc file can be used for local and different servers. Keeps the definition
      of experiment, dataset cache dir and others separate from the codebase.
      `direnv` exports the environment variables automatically on entering the root directory
    - ward is a new tool to run tests. pyproject.toml has the new dependency

---
+++ domadapter/datamodules/glue_dm.py
@@ -0,0 +1,200 @@
+import pytorch_lightning as pl
+from typing import Optional, Dict, List
+from rich.traceback import install
+from datasets import load_dataset
+from transformers import PreTrainedTokenizer
+from torch.utils.data import DataLoader
+
+# Pretty prints traceback in the console using `rich`
+install(show_locals=True)
+
+
+class GlueDM(pl.LightningDataModule):
+    def __init__(
+        self,
+        task_name: str,
+        dataset_cache_dir: str,
+        tokenizer: PreTrainedTokenizer,
+        overwrite_cache: bool = False,
+        pad_to_max_length: bool = True,
+        max_seq_length: int = None,
+        batch_size: int = 32,
+    ):
+        """Use the transformer datasets library to download
+        GLUE tasks. We should use this later if we decide to do experiments
+        on 10% data as used by the paper https://aclanthology.org/2021.acl-long.172/
+
+        Parameters
+        ----------
+        task_name: str
+            GLUE task name. One of
+            ["cola", "mnli", "mrpc", "qnli", "qqp", "rte", "sst2", "stsb", "wnli"]
+
+        dataset_cache_dir: str
+            Folder name stores the GLUE dataset downloaded from the web.
+            If downloaded already, use it from the directory
+            If `overwrite_cache` is True, then download every time and ignore
+            any downloaded versions
+
+        tokenizer: PreTrainedTokenizer
+            A pretrained tokenizer from the transformer library
+
+        overwrite_cache: bool
+            Download the dataset ignoring any downloaded versions?
+
+        pad_to_max_length: bool
+            Sets padding to True for the  hugging face tokenizer
+            https://huggingface.co/transformers/internal/tokenization_utils.html
+            Sets the padding to the longest sequence in the batch
+
+        max_seq_length: int
+            Controls the max_length parameter of the hugging face tokenizer
+            Controls the maximum length to use by one of the truncation/padding parameters.
+            If left unset or set to None, this will use the predefined model maximum length if a
+            maximum length is required by one of the truncation/padding parameters. If the model
+            has no specific maximum input length (like XLNet) truncation/padding to a maximum length
+            will be deactivated.
+
+        batch_size: int
+            Batch size of inputs
+        """
+        super(GlueDM, self).__init__()
+        self.task_to_keys = {
+            "cola": ("sentence", None),
+            "mnli": ("premise", "hypothesis"),
+            "mrpc": ("sentence1", "sentence2"),
+            "qnli": ("question", "sentence"),
+            "qqp": ("question1", "question2"),
+            "rte": ("sentence1", "sentence2"),
+            "sst2": ("sentence", None),
+            "stsb": ("sentence1", "sentence2"),
+            "wnli": ("sentence1", "sentence2"),
+        }
+
+        self.task_name = task_name
+        assert self.task_name in list(
+            self.task_to_keys.keys()
+        ), f"task_name should be one of {list(self.task_to_keys)}"
+
+        self.dataset_cache_dir = dataset_cache_dir
+        self.tokenizer = tokenizer
+        self.overwrite_cache = overwrite_cache
+        self.pad_to_max_length = pad_to_max_length
+        self.max_seq_length = max_seq_length
+
+        self.datasets = None  # Store the dataset
+        self.labels: List[str] = None  # Store the labels used
+        self.num_labels: int = None  # Stores the number of labels
+        self.label2id: Dict[str, int] = None  # Map from label to id
+        self.id2label: Dict[int, str] = None  # Map from id to label
+        self.train_dataset = None
+        self.val_dataset = None
+        self.test_dataset = None
+        self.batch_size = batch_size
+
+    def prepare_data(self):
+        """Download the dataset for the task and store it in the
+        dataset cache dir. We are not using the return value of
+        the `load_dataset` function because:
+        1. LightningDataModule does not recommend setting any properties inside this function
+        2. We want to just download in this function and use it when we call `setup`.
+
+        Returns
+        -------
+        None
+        """
+        load_dataset("glue", self.task_name, cache_dir=self.dataset_cache_dir)
+
+    def setup(self, stage: Optional[str] = None):
+
+        self.datasets = load_dataset("glue", self.task_name, cache_dir=self.dataset_cache_dir)
+
+        # Setup the labels
+        is_regression = self.task_name == "stsb"
+
+        if is_regression:
+            self.num_labels = 1
+
+        else:
+            # The features is a dictionary containing label key.
+            # The names property gives the names of the labels
+
+            self.labels = self.datasets["train"].features["label"].names
+            self.num_labels = len(self.labels)
+            label2id = dict(
+                [
+                    (label.lower(), self.datasets["train"].features["label"].str2int(label))
+                    for label in self.labels
+                ]
+            )
+            id2label = {v: k for k, v in label2id.items()}
+            self.id2label = id2label
+            self.label2id = label2id
+
+        # Tokenize the dataset
+        self.datasets = self.datasets.map(
+            self.preprocess_function, batched=True, load_from_cache_file=not self.overwrite_cache
+        )
+
+        # Return a pytorch tensor when the dataset is indexed like self.datasets[0]
+        self.datasets.set_format(
+            type="torch", columns=["input_ids", "token_type_ids", "attention_mask", "label"]
+        )
+
+        # Return the dataset
+
+        if stage == "fit":
+            self.train_dataset = self.datasets["train"]
+            self.val_dataset = (
+                self.datasets["validation_matched"]
+                if self.task_name == "mnli"
+                else self.datasets["validation"]
+            )
+
+        elif stage == "test":
+            self.test_dataset = (
+                self.datasets["test_matched"] if self.task_name == "mnli" else self.datasets["test"]
+            )
+        else:
+            raise ValueError("stage can be on of [fit, val, test]")
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.batch_size)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size=self.batch_size)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset, batch_size=self.batch_size)
+
+    def preprocess_function(self, examples):
+        sentence1_key, sentence2_key = self.task_to_keys[self.task_name]
+        args = (
+            (examples[sentence1_key],)
+            if sentence2_key is None
+            else (examples[sentence1_key], examples[sentence2_key])
+        )
+
+        result = self.tokenizer(
+            *args, padding=self.pad_to_max_length, max_length=self.max_seq_length, truncation=True
+        )
+
+        result["labels"] = examples["label"]
+
+        return result
+
+
+if __name__ == "__main__":
+    from transformers import AutoTokenizer
+
+    bert_tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+    dm = GlueDM(
+        task_name="cola",
+        dataset_cache_dir=".",
+        tokenizer=bert_tokenizer,
+    )
+    dm.prepare_data()
+    dm.setup("fit")
+    train_loader = dm.train_dataloader()
+    inputs = next(iter(train_loader))
+    attention_mask = inputs["attention_mask"]
---
+++ tests/datamodules/test_glue_dm.py
@@ -0,0 +1,54 @@
+from ward import test, fixture, Scope
+from domadapter.datamodules.glue_dm import GlueDM
+from transformers import AutoTokenizer
+from pathlib import Path
+import os
+
+TASK_NAMES = ["cola", "mnli", "mrpc", "qnli", "qqp", "rte", "sst2", "stsb", "wnli"]
+
+DATASETS_CACHE_DIR = Path(os.environ["DATASET_CACHE_DIR"]).joinpath("test_dataset_cache")
+
+
+@fixture
+def bert_tokenizer(scope=Scope.Module):
+    yield AutoTokenizer.from_pretrained("bert-base-uncased")
+
+
+for task in TASK_NAMES:
+
+    @test("prepare_data for {task_name}", tags=["unit", "data", "slow"])
+    def _(task_name=task, tokenizer=bert_tokenizer):
+        dm = GlueDM(task_name=task_name, dataset_cache_dir=DATASETS_CACHE_DIR, tokenizer=tokenizer)
+        dm.prepare_data()
+
+
+for task in TASK_NAMES:
+
+    @test("setup_data for {task_name}", tags=["unit", "data", "slow"])
+    def _(task_name=task, tokenizer=bert_tokenizer):
+        dm = GlueDM(task_name=task_name, dataset_cache_dir=DATASETS_CACHE_DIR, tokenizer=tokenizer)
+        dm.prepare_data()
+        dm.setup("fit")
+        dm.setup("test")
+        assert len(dm.train_dataset) > 0
+        assert len(dm.val_dataset) > 0
+        assert len(dm.test_dataset) > 0
+
+
+for task in TASK_NAMES:
+
+    @test("data loaders for {task_name}", tags=["unit", "data", "slow"])
+    def _(task_name=task, tokenizer=bert_tokenizer):
+        dm = GlueDM(
+            task_name=task_name,
+            dataset_cache_dir=DATASETS_CACHE_DIR,
+            tokenizer=tokenizer,
+            pad_to_max_length=True,
+            max_seq_length=32,
+        )
+        dm.prepare_data()
+        dm.setup("fit")
+        train_loader = dm.train_dataloader()
+        inputs = next(iter(train_loader))
+        attention_mask = inputs["attention_mask"]
+        assert attention_mask.size(0) > 0
