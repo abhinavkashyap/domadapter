8ffc158d128693d671980efd8834609072217948
Bhavitvya Malik <bhavitvya.malik@gmail.com>
2021-09-18 23:36:25 +0530

    minor changes, compute CMD of all hidden states

--- domadapter/models/domain_adapter.py
+++ domadapter/models/domain_adapter.py
@@ -1,35 +1,27 @@
 import torch
 import pytorch_lightning as pl
-from typing import Any, Optional, Dict, List, Union
-from transformers import BertModel
+from typing import Any, Optional, Dict
+from transformers import AutoModelWithHeads
 from transformers import BertConfig
-from transformers.models.bert.modeling_bert import BertPooler
 from domadapter.console import console
-import torch.nn as nn
-from torch.nn import CrossEntropyLoss
 import torch.optim as optim
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-from domadapter.models.linear_clf import LinearClassifier
-import numpy as np
-from domadapter.divergences.gaussian_mkmmd_divergence import GaussianMKMMD
-from domadapter.divergences.rbf_mkmmd_divergence import RBFMKMMD
 from domadapter.divergences.cmd_divergence import CMD
 
 
 class DomainAdapter(pl.LightningModule):
 
-    def __init__(self, config, hparams:Optional[Dict[str, Any]] = None):
-        """Domain Adapter LightningModule to train domain adapter using cmd as divernnce
-
+    def __init__(self, hparams:Optional[Dict[str, Any]] = None):
+        """Domain Adapter LightningModule to train domain adapter using CMD as divergence.
         Args:
             hparams (Optional[Dict[str, Any]], optional): [description]. Defaults to None.
         """
         super(DomainAdapter, self).__init__()
 
-        self.hparams = hparams
-        self.config = config
 
-        # bert config 
+        self.save_hyperparameters(hparams)
+
+        # bert config
         self.bert_config = (
             BertConfig()
             if self.hparams.get("bert_config") is None
@@ -40,53 +32,46 @@ class DomainAdapter(pl.LightningModule):
 
         # load the bert model weights
         with console.status("Loading BERT Model", spinner="monkey"):
-            self.bert = BertModel.from_pretrained(
+            self.bert = AutoModelWithHeads.from_pretrained(
                 self.pretrained_model_name, config=self.bert_config
             )
         console.print(
             f"[green] Loaded BERT model from {self.hprams['pretrained_model_name']}"
         )
 
-        # add adapter a new adapter 
-        self.bert.add_adapter(self.config['domain_adapter_name'])
-
+        # add adapter a new adapter
+        self.bert.add_adapter(self.hprams['domain_adapter_name'])
         # activate the adapter
-        self.bert.train_adapter(self.config['domain_adapter_name'])
-
-
+        self.bert.train_adapter(self.hprams['domain_adapter_name'])
         # object to compute the divergence
         self.criterion = CMD()
-
 
 
     def forward(self, input_ids, attention_mask=None):
         """Forward pass of the model"""
         # get the bert output
         bert_output = self.bert(input_ids=input_ids, attention_mask=attention_mask)
+        hidden_states = bert_output.hidden_states
+        return hidden_states
 
-        # get the last layer pooler output
-        pooler_output = bert_output.pooler_output
-
-        return pooler_output
-    
 
     # def configure_optimizers(self):
     #     return torch.optim.AdamW(
-    #         params=self.model.parameters(), 
-    #         lr=self.hparams['learning_rate'], 
-    #         betas=self.hparams['betas'], 
-    #         eps=self.hparams['eps'], 
+    #         params=self.model.parameters(),
+    #         lr=self.hparams['learning_rate'],
+    #         betas=self.hparams['betas'],
+    #         eps=self.hparams['eps'],
     #         weight_decay=self.hparams['weight_decay'],
     #         # amsgrad=self.hparams['amsgrad'], not using this hparam
     #     )
-        
+
     def configure_optimizers(self):
-        
+
         optimizer = torch.optim.AdamW(
-            params=self.model.parameters(), 
-            lr=self.hparams['learning_rate'], 
-            betas=self.hparams['betas'], 
-            eps=self.hparams['eps'], 
+            params=self.model.parameters(),
+            lr=self.hparams['learning_rate'],
+            betas=self.hparams['betas'],
+            eps=self.hparams['eps'],
             weight_decay=self.hparams['weight_decay'],
             # amsgrad=self.hparams['amsgrad'], not using this hparam
         )
@@ -114,16 +99,16 @@ class DomainAdapter(pl.LightningModule):
         )
 
     def training_step(self, batch, batch_idx):
-
-        BATCH_SIZE = batch['input_ids'].shape[0]
-
         # concat the source and target data and pass it to the bert model
         input_ids = torch.cat((batch["source_input_ids"], batch["target_input_ids"]), axis=0)
         attention_mask = torch.cat((batch["source_attention_mask"], batch["target_attention_mask"]), axis=0)
 
         outputs = self(input_ids=input_ids, attention_mask=attention_mask)
-        src_feature, trg_feature = torch.split(tensor=outputs, split_size_or_sections=BATCH_SIZE, dim=0)
-        divergence = self.criterion.calculate(src_hidden=src_feature, trg_hidden=trg_feature)
+
+        divergence = 0
+        for num in range(len(outputs)):
+            src_feature, trg_feature = torch.split(tensor=outputs[num], split_size_or_sections=batch['input_ids'].shape[0]//2, dim=0)
+            divergence += self.criterion.calculate(src_hidden=src_feature, trg_hidden=trg_feature)
 
         self.log(
             "train/divergence",
@@ -137,15 +122,16 @@ class DomainAdapter(pl.LightningModule):
 
     def validation_step(self, batch, batch_idx):
 
-        BATCH_SIZE = batch['input_ids'].shape[0]
-
         # concat the source and target data and pass it to the bert model
         input_ids = torch.cat((batch["source_input_ids"], batch["target_input_ids"]), axis=0)
         attention_mask = torch.cat((batch["source_attention_mask"], batch["target_attention_mask"]), axis=0)
 
         outputs = self(input_ids=input_ids, attention_mask=attention_mask)
-        src_feature, trg_feature = torch.split(tensor=outputs, split_size_or_sections=BATCH_SIZE, dim=0)
-        divergence = self.criterion.calculate(src_hidden=src_feature, trg_hidden=trg_feature)
+
+        divergence = 0
+        for num in range(len(outputs)):
+            src_feature, trg_feature = torch.split(tensor=outputs[num], split_size_or_sections=batch['input_ids'].shape[0]//2, dim=0)
+            divergence += self.criterion.calculate(src_hidden=src_feature, trg_hidden=trg_feature)
 
         # we can comment the logging here
         self.log(
@@ -159,10 +145,9 @@ class DomainAdapter(pl.LightningModule):
         return {"val/divergence": divergence}
 
     def validation_epoch_end(self, outputs):
-
         mean_divergenence = torch.stack([x['val/divergence'] for x in outputs]).mean()
 
-        # this will show the mean div value across epoch 
+        # this will show the mean div value across epoch
         self.log(
             "val/divergence",
             value=mean_divergenence,
@@ -174,38 +159,6 @@ class DomainAdapter(pl.LightningModule):
         # need not to return
         # return {"val/divergence": mean_divergenence}
 
-    
-    def test_step(self, batch, batch_idx):
-
-        BATCH_SIZE = batch['input_ids'].shape[0]
-
-        # concat the source and target data and pass it to the bert model
-        input_ids = torch.cat((batch["source_input_ids"], batch["target_input_ids"]), axis=0)
-        attention_mask = torch.cat((batch["source_attention_mask"], batch["target_attention_mask"]), axis=0)
-
-        outputs = self(input_ids=input_ids, attention_mask=attention_mask)
-        src_feature, trg_feature = torch.split(tensor=outputs, split_size_or_sections=BATCH_SIZE, dim=0)
-        divergence = self.criterion.calculate(src_hidden=src_feature, trg_hidden=trg_feature)
-
-    
-        return {"test/divergence": divergence}
-
-    
-    def test_epoch_end(self, outputs):
-        # compute the mean of divergence across epoch and return it
-        mean_divergence = torch.stack([x['test/divergence'] for x in outputs]).mean()
-
-        # log it
-        self.log(
-            "test/divergence",
-            value=mean_divergence,
-            prog_bar=False,
-            on_step=False,
-            logger=True,
-            on_epoch=True,
-        )
-        return {"test/divergence": mean_divergence}
-        
 
 
 
